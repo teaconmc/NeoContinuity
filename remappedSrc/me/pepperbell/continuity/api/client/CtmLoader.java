7603c4ccf7e16ae33c22545f5cba051e063f2214
Argon4W <1516775492@qq.com>
2024-12-12 20:52:46 +0800

    Remove remappedSrc

--- remappedSrc/me/pepperbell/continuity/api/client/CtmLoader.java
+++
@@ -1,9 +0,0 @@
-package me.pepperbell.continuity.api.client;
-
-public interface CtmLoader<T extends CtmProperties> {
-	CtmProperties.Factory<T> getPropertiesFactory();
-
-	QuadProcessor.Factory<T> getProcessorFactory();
-
-	CachingPredicates.Factory<T> getPredicatesFactory();
-}
